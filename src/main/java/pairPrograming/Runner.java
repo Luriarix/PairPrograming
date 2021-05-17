22005ea411b0b0d37df1d8bd1785a66a6bd9e19b
Luriarix <ionitaandrei_lucian@yahoo.ro>
2021-05-17 13:04:16 +0100

    goldy

--- src/main/java/pairPrograming/Runner.java
+++ src/main/java/pairPrograming/Runner.java
@@ -1,5 +1,8 @@
 package pairPrograming;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Runner {
 	
 	
@@ -16,7 +19,47 @@ public class Runner {
 		}
 		return Integer.toString(factorial) + '!';
 	}
-	public void main(String[] args) {
+	
+	public String doggo(int winner) {
+		String positions = "";
+		
+		for (int i = 1; i <= 100; i++) {
+			if(i == winner) {
+				continue;
+			}
+			if ((i < 10 || i > 20) && (i % 10 == 1 || i % 10 == 2 || i % 10 == 3)) {
+				if (i % 10 == 1) {
+					positions += Integer.toString(i) + "st";
+				} else if (i % 10 == 2) {
+					positions += Integer.toString(i) + "nd";
+				} else if (i % 10 == 3) {
+					positions += Integer.toString(i) + "rd";
+				}
+				
+			} else {
+				positions += Integer.toString(i) + "th";
+			}
+			
+			if (i != 100) {
+				positions += ',';				
+			}
+		}
+		return positions;
 	}
+	
+	public List<Integer> goldy(int[] chair, int[] temerature) {
+		List<Integer> possibleChairs = new ArrayList<>();
+		int goldyWeight = 100;
+		int goldyTemperature = 120;
+		
+		for (int i = 0; i < chair.length; i++) {
+			if(goldyWeight <= chair[i] && goldyTemperature >= temerature[i]) {
+				possibleChairs.add(i);
+			}
+		}
+		return possibleChairs;
+		
+	}
+
 	
 }
--- src/test/java/pairPrograming/RunnerTest.java
+++ src/test/java/pairPrograming/RunnerTest.java
@@ -18,5 +18,17 @@ public class RunnerTest {
 		assertEquals("NONE", runner.factorial(18));
 	}
 	
+	@Test
+	public void dogoTest() {
+		System.out.println(runner.doggo(5));
+	
+	}
+	
+	@Test
+	public void goldyTest() {
+		int[] chairs = {297,66,257,276,280,163,193,153,107,137,40,127,146,197,59,124,59,54,119,121,189,108,21,18,90,52,129,181,123,132};
+		int[] temperature = {90,110,113,191,129,163,193,153,107,137,60,127,146,197,59,124,59,54,119,121,189,108,21,18,90,52,129,181,123,132};
+ 		System.out.println(runner.goldy(chairs, temperature));
+	}
 	
 }
